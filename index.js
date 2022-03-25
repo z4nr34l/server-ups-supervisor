b434399018966d98f6b510b702bcbefa9bac876d
Mateusz Janota <software@zanreal.pl>
2022-03-25 15:14:27 +0100

    - Migrated UPS network settings to .env; - Added discord webhook for notifications;

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
  * Author URI: https://www.zanreal.pl/
  * Author email: software@zanreal.pl
  */
+require('dotenv').config()
 
 const hosts = require('./hosts.json')
 const adapter = require('ever-ups-adapter')
-const {NodeSSH} = require('node-ssh')
+const { NodeSSH } = require('node-ssh')
+const { MessageEmbed, WebhookClient } = require("discord.js");
 const ssh = new NodeSSH()
+const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL });
 
 /**
  * SNMP values mapping
@@ -24,10 +27,50 @@ const outputSourceMapping = {
   7: "Reducer"
 }
 
+const messageTypeColorMapping = {
+  "info": "#0284c7",
+  "success": "#65a30d",
+  "warning": "#ea580c",
+  "error": "#dc2626"
+}
+
 let oldState = undefined;
+let messageId = undefined;
+let pendingActions = false;
 
 if (!hosts) {
   console.error("Please create hosts.json file and provide at least one host data in it.")
+}
+
+function sendWebhookStateChangeMessage(oldState, currentState, batteryPercentage, type, customMessage) {
+
+  const embed = new MessageEmbed()
+    .setColor(messageTypeColorMapping[type])
+    .setTitle('UPS Supervisor')
+    .setDescription('Detected UPS state change.')
+    .addFields(
+      customMessage,
+      { name: 'Previous state', value: oldSate, inline: true },
+      { name: 'Current state', value: currentState, inline: true },
+      { name: 'Battery %', value: batteryPercentage, inline: true }
+    )
+    .setTimestamp()
+    .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
+
+  if(!messageId) {
+    webhookClient.send({
+      username: 'UPS Supervisor',
+      avatarURL: 'https://www.zanreal.pl/avatar.png',
+      embeds: [embed]
+    }).then(message => messageId = message.id)
+  } else {
+    webhookClient.editMessage(messageId, {
+      username: 'UPS Supervisor',
+      avatarURL: 'https://www.zanreal.pl/avatar.png',
+      embeds: [embed]
+    })
+  }
+
 }
 
 /**
@@ -75,7 +118,8 @@ function emergencyPowerOff() {
 async function Main() {
   let timeoutHandle = undefined
   const session = adapter.createSession({
-    address: "10.0.0.222"
+    address: process.env.UPS_IP,
+    community: process.env.UPS_COMMUNITY
   }, {
     verbose: true
   })
@@ -90,13 +134,19 @@ async function Main() {
             console.warn(`Power state changed! (${outputSourceMapping[currentState]})`)
 
             if(oldState === 3 && currentState === 5) {
+              sendWebhookStateChangeMessage(oldState, currentState, 100, "warning", { name: '***WARNING***', value: 'VMs and hosts will be shut down in 30 seconds!' })
               console.warn("WARNING! Hosts will be powered off in 30 seconds")
+              pendingActions = true
               timeoutHandle = setTimeout(()=> {
+                sendWebhookStateChangeMessage(oldState, currentState, 100, "error", { name: '***WARNING***', value: 'VMs and hosts are going to shut down!' })
                 emergencyPowerOff()
               }, 30000)
             }
             if(oldState === 5 && currentState === 3) {
               clearTimeout(timeoutHandle)
+              pendingActions = false
+              sendWebhookStateChangeMessage(oldState, currentState, 100, "success", null)
+              messageId = undefined
               console.warn("Hosts poweroff was canceled due to power recovery")
             }
 
