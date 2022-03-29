4b0b1f3642498c78ba16413c7554162f8ab19a76
Mateusz Janota <software@zanreal.pl>
2022-03-29 09:43:22 +0200

    - Added more Discord webhook notifications - Failsafe emergency function timeout time is moved to .env (Default: 3min, should be provided in ms)

--- index.js
+++ index.js
@@ -34,37 +34,91 @@ const messageTypeColorMapping = {
   "error": "#dc2626"
 }
 
-let oldState = undefined;
-let messageId = undefined;
-let pendingActions = false;
+let oldState = undefined
+let messageId = undefined
+let initMessageId = undefined
+let fistStatusMessageSending = false
+let pendingActions = false
 
 if (!hosts) {
   console.error("Please create hosts.json file and provide at least one host data in it.")
 }
 
-function sendWebhookStateChangeMessage(oldState, currentState, batteryPercentage, type, customMessage) {
+const preInitMessage = new MessageEmbed()
+  .setColor(messageTypeColorMapping['info'])
+  .setTitle('UPS Supervisor')
+  .setDescription('Initializing UPS Supervisor')
+  .setTimestamp()
+  .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
 
-  const embed = new MessageEmbed()
-    .setColor(messageTypeColorMapping[type])
+const postInitMessage = new MessageEmbed()
+  .setColor(messageTypeColorMapping['success'])
+  .setTitle('UPS Supervisor')
+  .setDescription('UPS Supervisor initialized and armed')
+  .addFields(
+    { name: 'Hosts', value: `${hosts.length}`, inline: true },
+    { name: 'UPS IP', value: `${process.env.UPS_IP}`, inline: true }
+  )
+  .setTimestamp()
+  .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
+
+function sendWebhookHostPoweroffMessage(host) {
+  const hostPoweroffMessage = new MessageEmbed()
+    .setColor(messageTypeColorMapping['error'])
     .setTitle('UPS Supervisor')
-    .setDescription('Detected UPS state change.')
+    .setDescription('Powering off host')
     .addFields(
-      customMessage,
-      { name: 'Previous state', value: oldSate, inline: true },
-      { name: 'Current state', value: currentState, inline: true },
-      { name: 'Battery %', value: batteryPercentage, inline: true }
+      { name: 'Host', value: host, inline: true }
     )
     .setTimestamp()
     .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
 
+  webhookClient.send({
+    username: 'UPS Supervisor',
+    avatarURL: 'https://www.zanreal.pl/avatar.png',
+    embeds: [hostPoweroffMessage]
+  })
+}
+
+async function sendWebhookStateChangeMessage(oldState, currentState, batteryPercentage, type, customMessage) {
+
+  let embed = undefined
+
+  if(customMessage) {
+    embed = new MessageEmbed()
+      .setColor(messageTypeColorMapping[type])
+      .setTitle('UPS Supervisor')
+      .setDescription('Detected UPS state change.')
+      .addFields(
+        customMessage,
+        { name: 'Previous state', value: `${outputSourceMapping[oldState]}`, inline: true },
+        { name: 'Current state', value: `${outputSourceMapping[currentState]}`, inline: true },
+        { name: 'Battery %', value: `${batteryPercentage}`, inline: true }
+      )
+      .setTimestamp()
+      .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
+  } else {
+    embed = new MessageEmbed()
+      .setColor(messageTypeColorMapping[type])
+      .setTitle('UPS Supervisor')
+      .setDescription('Detected UPS state change.')
+      .addFields(
+        { name: 'Previous state', value: `${outputSourceMapping[oldState]}`, inline: true },
+        { name: 'Current state', value: `${outputSourceMapping[currentState]}`, inline: true },
+        { name: 'Battery %', value: `${batteryPercentage}`, inline: true }
+      )
+      .setTimestamp()
+      .setFooter({ text: 'by Z4NR34L', iconURL: 'https://www.zanreal.pl/avatar.png' });
+  }
+
   if(!messageId) {
-    webhookClient.send({
+    await webhookClient.send({
       username: 'UPS Supervisor',
       avatarURL: 'https://www.zanreal.pl/avatar.png',
       embeds: [embed]
     }).then(message => messageId = message.id)
   } else {
-    webhookClient.editMessage(messageId, {
+    await webhookClient.editMessage(messageId, {
       username: 'UPS Supervisor',
       avatarURL: 'https://www.zanreal.pl/avatar.png',
       embeds: [embed]
@@ -79,6 +133,7 @@ function sendWebhookStateChangeMessage(oldState, currentState, batteryPercentage
 function emergencyPowerOff() {
   hosts.every(host => {
     console.log(`Powering-off host: ${host.name} (${host.ip_address})`)
+    sendWebhookHostPoweroffMessage(host.ip_address)
     let sshConfig = {}
     if (host.private_key) {
       sshConfig = {
@@ -116,9 +171,16 @@ function emergencyPowerOff() {
  * @constructor
  */
 async function Main() {
+
+  await webhookClient.send({
+    username: 'UPS Supervisor',
+    avatarURL: 'https://www.zanreal.pl/avatar.png',
+    embeds: [preInitMessage]
+  }).then(message => initMessageId = message.id)
+
   let timeoutHandle = undefined
   const session = adapter.createSession({
-    address: process.env.UPS_IP,
+    address: '10.5.0.254',
     community: process.env.UPS_COMMUNITY
   }, {
     verbose: true
@@ -130,22 +192,26 @@ async function Main() {
         const currentState = data.output_source.value
 
         if(oldState) {
+          if(oldState === 3 && !messageId && !fistStatusMessageSending) {
+            fistStatusMessageSending = true
+            sendWebhookStateChangeMessage(oldState, currentState, data.estimated_battery_percentage.value, "success", undefined)
+          }
           if(currentState !== oldState) {
             console.warn(`Power state changed! (${outputSourceMapping[currentState]})`)
 
             if(oldState === 3 && currentState === 5) {
-              sendWebhookStateChangeMessage(oldState, currentState, 100, "warning", { name: '***WARNING***', value: 'VMs and hosts will be shut down in 30 seconds!' })
+              sendWebhookStateChangeMessage(oldState, currentState, data.estimated_battery_percentage.value, "warning", { name: '***WARNING***', value: 'VMs and hosts will be shut down in 30 seconds!' })
               console.warn("WARNING! Hosts will be powered off in 30 seconds")
               pendingActions = true
               timeoutHandle = setTimeout(()=> {
-                sendWebhookStateChangeMessage(oldState, currentState, 100, "error", { name: '***WARNING***', value: 'VMs and hosts are going to shut down!' })
+                sendWebhookStateChangeMessage(oldState, currentState, data.estimated_battery_percentage.value, "error", { name: '***WARNING***', value: 'VMs and hosts are going to shut down!' })
                 emergencyPowerOff()
-              }, 30000)
+              }, process.env.FAILSAFE_DELAY || 180000)
             }
             if(oldState === 5 && currentState === 3) {
               clearTimeout(timeoutHandle)
               pendingActions = false
-              sendWebhookStateChangeMessage(oldState, currentState, 100, "success", null)
+              sendWebhookStateChangeMessage(oldState, currentState, data.estimated_battery_percentage.value, "success", undefined)
               messageId = undefined
               console.warn("Hosts poweroff was canceled due to power recovery")
             }
@@ -160,6 +226,14 @@ async function Main() {
       error => console.error(error)
     )
   }, 160 )
+
+  if(initMessageId) {
+    await webhookClient.editMessage(initMessageId, {
+      username: 'UPS Supervisor',
+      avatarURL: 'https://www.zanreal.pl/avatar.png',
+      embeds: [postInitMessage]
+    })
+  }
 }
 
 /**
